b449d99becc5d3e8e13c940e8150b7ba62e57a98
alejandroquesada <quesadaaba@gmail.com>
2018-01-02 18:00:36 -0500

    updated more examples running in container.

---
+++ examples/vanilla-js-protractor/conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  multiCapabilities: [{
+      'browserName': 'chrome',
+      chromeOptions: {
+        args: [
+          "--headless",
+          "no-sandbox",
+          "--disable-gpu",
+          "--window-size=1200,950"
+        ]
+      }
+    }
+  ],
+  specs: ['spec.js']
+}
